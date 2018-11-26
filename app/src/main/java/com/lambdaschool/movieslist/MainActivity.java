0f0382385fedf24ce75564470a7970be9a323af8
Chance Payne <chance@lambdaschool.com>
2018-11-26 12:13:20 -0700

    add simple thread to project

--- app/src/main/java/com/lambdaschool/movieslist/MainActivity.java
+++ app/src/main/java/com/lambdaschool/movieslist/MainActivity.java
@@ -1,19 +1,25 @@
 package com.lambdaschool.movieslist;
 
 import android.app.Activity;
+import android.arch.lifecycle.LifecycleOwner;
 import android.arch.lifecycle.Observer;
 
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
+import com.lambdaschool.movieslist.apiaccess.MovieDbDao;
 import com.lambdaschool.movieslist.apiaccess.MovieOverview;
 
 
@@ -22,10 +28,11 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    LinearLayout           parentLayout;
+    ScrollView             parentLayout;
     MovieOverviewViewModel viewModel;
     Context                context;
     Activity               activity;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
         ThemeUtils.onActivityCreateSetTheme(activity);
         setContentView(R.layout.activity_main);
 
-        parentLayout = findViewById(R.id.parent_layout);
+        parentLayout = findViewById(R.id.scroll_view);
         context = this;
 
         findViewById(R.id.toggle_button).setOnClickListener(new View.OnClickListener() {
@@ -43,18 +50,28 @@ public class MainActivity extends AppCompatActivity {
                 ThemeUtils.toggleTheme(activity);
             }
         });
+        progressBar = findViewById(R.id.progress_bar);
 
-        viewModel = ViewModelProviders.of(this).get(MovieOverviewViewModel.class);
-        viewModel.getOverViewList().observe(this, new Observer<ArrayList<MovieOverview>>() {
+        (new GetMoviesTask()).execute();
+
+        /*(new Thread(new Runnable() {
             @Override
-            public void onChanged(@Nullable ArrayList<MovieOverview> movieOverviews) {
-                if(movieOverviews != null) {
-                    for(MovieOverview movie: movieOverviews) {
-                        parentLayout.addView(getDefaultTextView(movie));
+            public void run() {
+                viewModel = ViewModelProviders.of((FragmentActivity) activity).get(MovieOverviewViewModel.class);
+                viewModel.getOverViewList().observe((LifecycleOwner) activity, new Observer<ArrayList<MovieOverview>>() {
+                    @Override
+                    public void onChanged(@Nullable ArrayList<MovieOverview> movieOverviews) {
+                        if(movieOverviews != null) {
+                            for(MovieOverview movie: movieOverviews) {
+                                parentLayout.addView(getDefaultTextView(movie));
+                            }
+                        }
                     }
-                }
+                });
             }
-        });
+        })).start();*/
+
+
     }
 
     TextView getDefaultTextView(final MovieOverview movie) {
@@ -75,5 +92,47 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         return view;
+    }
+
+    public class GetMoviesTask extends AsyncTask<Void, Integer, View> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressBar.setVisibility(View.VISIBLE);
+            progressBar.setMax(25);
+        }
+
+        @Override
+        protected void onPostExecute(View view) {
+            super.onPostExecute(view);
+            progressBar.setVisibility(View.GONE);
+            parentLayout.addView(view);
+        }
+
+        @Override
+        protected View doInBackground(Void... voids) {
+            ArrayList<MovieOverview> movieOverviews = MovieDbDao.getPopularMovies();
+            LinearLayout view = new LinearLayout(context);
+            view.setOrientation(LinearLayout.VERTICAL);
+            if(movieOverviews != null) {
+                for(int i = 0; i < movieOverviews.size(); ++i) {
+                    (view).addView(getDefaultTextView(movieOverviews.get(i)));
+                    publishProgress(i+5);
+                    try {
+                        Thread.sleep(250);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            return view;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+//            parentLayout.addView(values[0]);
+            progressBar.setProgress(values[0]);
+        }
     }
 }
